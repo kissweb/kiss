2bdee4a8e521746f3ebf3822405f1edb3422b0a0
Jonathan Ong <jonathanrichardong@gmail.com>
2015-02-15 22:04:06 -0800

    C R E A T I O N

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# kiss
+
+[![Gitter chat][gitter-image]][gitter-url]
+[![NPM version][npm-image]][npm-url]
+[![Latest tag][github-tag]][github-url]
+[![Build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![Dependency Status][david-image]][david-url]
+[![License][license-image]][license-url]
+[![Downloads][downloads-image]][downloads-url]
+[![Gittip][gittip-image]][gittip-url]
+
+[gitter-image]: https://badges.gitter.im/jonathanong/kiss.png
+[gitter-url]: https://gitter.im/jonathanong/kiss
+[npm-image]: https://img.shields.io/npm/v/kiss.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/kiss
+[github-tag]: http://img.shields.io/github/tag/jonathanong/kiss.svg?style=flat-square
+[github-url]: https://github.com/jonathanong/kiss/tags
+[travis-image]: https://img.shields.io/travis/jonathanong/kiss.svg?style=flat-square
+[travis-url]: https://travis-ci.org/jonathanong/kiss
+[coveralls-image]: https://img.shields.io/coveralls/jonathanong/kiss.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/jonathanong/kiss
+[david-image]: http://img.shields.io/david/jonathanong/kiss.svg?style=flat-square
+[david-url]: https://david-dm.org/jonathanong/kiss
+[license-image]: http://img.shields.io/npm/l/kiss.svg?style=flat-square
+[license-url]: LICENSE
+[downloads-image]: http://img.shields.io/npm/dm/kiss.svg?style=flat-square
+[downloads-url]: https://npmjs.org/package/kiss
+[gittip-image]: https://img.shields.io/gratipay/jonathanong.svg?style=flat-square
+[gittip-url]: https://gratipay.com/jonathanong/
---
+++ index.js
@@ -0,0 +1,235 @@
+'use strict'
+
+/**
+ * TODO:
+ *
+ *   - toggle hidden file support
+ *   - CORS
+ *   - middleware
+ *
+ */
+
+const resolve = require('resolve-path')
+const rawBody = require('raw-body')
+const assert = require('assert')
+const path = require('path')
+const etag = require('etag')
+const fs = require('mz/fs')
+const ms = require('ms')
+
+module.exports = Kiss
+
+function Kiss(options) {
+  if (!(this instanceof Kiss)) return new Kiss(options)
+
+  this.options = options || {}
+
+  this._folders = []
+}
+
+/**
+ * Pretend Kiss is a generator function so `app.use()` works.
+ */
+
+Kiss.prototype.constructor = Object.getPrototypeOf(function* () {}).constructor
+
+/**
+ * Support .call().
+ */
+
+Kiss.prototype.call = function* (context, next) {
+  yield* next
+
+  // push dependencies
+  yield* this.serveDependencies(context, next)
+
+  // response is already handled
+  let res = context.response
+  if (res.body || res.status !== 404) return
+
+  // serve the response
+  yield* this.serve(context, next)
+}
+
+/**
+ * Push the current request's dependencies.
+ */
+
+Kiss.prototype.serveDependencies = function* (context, next) {
+  let req = context.request
+  let res = context.response
+  let type = req.is('html', 'css', 'js')
+  if (!type) return
+
+  let body = res.body
+  if (body == null) return // why would this happen?
+  if (Buffer.isBuffer(body)) body = body.toString()
+  if (body._readableState) body = yield rawBody(body, { encoding: 'utf8' })
+  yield this.pushDependencies(req.path, type, body)
+}
+
+/**
+ * Serve a file as the response to the request.
+ * Note: assumes you're using your own compression middleware.
+ */
+
+Kiss.prototype.serve = function* (context, next) {
+  let req = context.request
+  let pathname = req.path
+  let stats = yield* this.lookup(pathname)
+  if (!stats) return yield* next
+
+  let res = context.response
+  if (stats.mtime instanceof Date) res.lastModified = stats.mtime
+  if (typeof stats.size === 'number') res.length = stats.size
+  res.type = stats.type || path.extname(pathname)
+  res.etag = yield this.etag(stats)
+
+  let fresh = req.fresh
+  switch (req.method) {
+    case 'HEAD': return res.status = fresh ? 304 : 200
+    case 'GET': break
+    case 'OPTIONS':
+      res.set('Allow', 'OPTIONS,HEAD,GET')
+      res.status = 204
+      return
+    default:
+      res.set('Allow', 'OPTIONS,HEAD,GET')
+      res.status = 405
+      return
+  }
+
+  if (fresh) return res.status = 304
+
+  // push dependencies
+  let type = res.is('html', 'css', 'js')
+  if (type) {
+    // TODO: cache this in production or something
+    let body = res.body = stats.body != null
+      ? stats.body
+      : (yield fs.readFile(stats.filename, 'utf8'))
+    yield this.pushDependencies(stats.pathname, type, body)
+  }
+
+  res.body = res.body || stats.body != null
+    ? stats.body
+    : fs.createReadStream(stats.filename)
+}
+
+/**
+ * Mount a path as a static server.
+ * Like Express's `.use()`, except `.use()` in this instance
+ * will be reserved for plugins.
+ */
+
+Kiss.prototype.mount = function (prefix, folder) {
+  if (typeof folder !== 'string') {
+    // no prefix
+    folder = prefix
+    prefix = '/'
+  }
+
+  assert(prefix[0] === '/', 'Mounted paths must begin with a `/`.')
+  if (!/\/$/.test(prefix)) prefix += '/'
+
+  this._folders.push(prefix, path.resolve(folder))
+  return this
+}
+
+/**
+ * Use plugins and middleware for this server.
+ */
+
+Kiss.prototype.use = function () {
+  throw new Error('Not implemented.')
+}
+
+/**
+ * Alias a path as another path.
+ * Essentially a symlink.
+ * Should support both files and folders.
+ */
+
+Kiss.prototype.alias = function () {
+  throw new Error('Not implemented.')
+}
+
+/**
+ * Define a custom, virtual path.
+ * Ex. kiss.define('/polyfill.js', (context, next) -> [stats])
+ */
+
+Kiss.prototype.define = function () {
+  throw new Error('Not implemented.')
+}
+
+/**
+ * Lookup a web path such as `/file/index.js` based on all the support paths.
+ */
+
+Kiss.prototype.lookup = function* (pathname) {
+  for (let pair of this._folders) {
+    let prefix = pair[0]
+    if (pathname.indexOf(prefix) !== 0) continue
+    let folder = pair[1]
+    let filename = resolve(folder, pathname.replace(prefix, ''))
+    let stats = yield fs.lstat(filename).catch(ignoreENOENT)
+    if (!stats) continue
+    if (!stats.isFile()) continue
+    stats.mount = pair
+    stats.filename = filename
+    stats.pathname = pathname
+    return stats
+  }
+}
+
+/**
+ * Set the etag function with caching.
+ * Assumes the etag function is always async.
+ */
+
+Kiss.prototype.etag = function (fn) {
+  assert(typeof fn === 'function')
+  this._etag = function (stats) {
+    return Promise.resolve(stats.etag || (stats.etag = fn(stats)))
+  }
+  return this
+}
+
+/**
+ * Set the default etag function.
+ */
+
+Kiss.prototype.etag(function (stats) {
+  return etag(stats, {
+    weak: true
+  })
+})
+
+/**
+ * Set the cache control.
+ * TODO: support actual cache control headers
+ */
+
+Kiss.prototype.cacheControl = function (age) {
+  // human readable string to ms
+  if (typeof age === 'string') age = ms(age)
+  // ms to seconds
+  age = Math.round(age / 1000)
+  this._cacheControl = 'public, max-age=' + age
+  return this
+}
+
+/**
+ * Set the default cache control.
+ */
+
+Kiss.prototype.cacheControl(31536000000)
+
+/**
+ * Ignore missing file errors on `.stat()`
+ */
+
+function ignoreENOENT(err) {
+  if (err.code !== 'ENOENT') throw err
+}
